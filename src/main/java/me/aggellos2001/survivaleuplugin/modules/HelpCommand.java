664b40f25b8781e8ea08209f506c71b6c57d788d
aggellos2001 <aggellos2001@gmail.com>
2021-01-27 22:56:10 +0200

    Fixed a logic error with help command

--- src/main/java/me/aggellos2001/survivaleuplugin/modules/HelpCommand.java
+++ src/main/java/me/aggellos2001/survivaleuplugin/modules/HelpCommand.java
@@ -3,6 +3,10 @@ package me.aggellos2001.survivaleuplugin.modules;
 import co.aikar.commands.annotation.CommandAlias;
 import me.aggellos2001.survivaleuplugin.utils.PluginActivity;
 import me.aggellos2001.survivaleuplugin.utils.Utilities;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -12,9 +16,9 @@ public final class HelpCommand extends PluginActivity {
 
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	private void onServerHelp(final PlayerCommandPreprocessEvent e) {
-		if (e.getMessage().toLowerCase().startsWith("/help")) {
+		if (e.getMessage().equalsIgnoreCase("/help")) {
 			e.setCancelled(true);
-			Utilities.sendMsg(e.getPlayer(), "&6&lhttps://survivaleu.com/documentation/");
+			Utilities.sendMsg(e.getPlayer(),"&e&lhttps://survivaleu.com/docs.html");
 		}
 	}
 }
